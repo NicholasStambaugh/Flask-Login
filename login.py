78968276da62cffda4cb0f90075de2dfd8847a93
Nick Stambaugh <73977662+NicholasStambaugh@users.noreply.github.com>
2023-04-27 14:55:39 -0400

    Add files via upload

---
+++ login.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'password':
+            return redirect(url_for('dashboard'))
+        else:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
