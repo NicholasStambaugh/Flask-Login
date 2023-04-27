273e321524bbee33cd24a007560d486183371307
Nick Stambaugh <73977662+NicholasStambaugh@users.noreply.github.com>
2023-04-27 15:17:35 -0400

    Update backend.php
    added login failsafe for brute force attacks

--- backend.php
+++ backend.php
@@ -7,6 +7,7 @@ $password = $_POST['password'];
 
 // TODO: Add database connection code here
 
+
 // Check if the username and password are valid
 if ($username === 'admin' && $password === 'password') {
   $_SESSION['username'] = $username;
@@ -14,5 +15,17 @@ if ($username === 'admin' && $password === 'password') {
 } else {
   $error = 'Invalid username or password';
   header('Location: index.php?error=' . urlencode($error));
+}
+
+// Count the number of login attempts for this username
+$attempts_query = "SELECT COUNT(*) AS attempts FROM login_attempts WHERE username = '$username'";
+$attempts_result = mysqli_query($conn, $attempts_query);
+$attempts_row = mysqli_fetch_assoc($attempts_result);
+$attempts = $attempts_row['attempts'];
+
+// If there have been 3 failed login attempts for this username, lock the account
+if ($attempts >= 3) {
+  $lock_query = "UPDATE users SET locked = 1 WHERE username = '$username'";
+  mysqli_query($conn, $lock_query);
 }
 ?>
